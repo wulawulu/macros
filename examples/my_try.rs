c7140dd341dc4045c14d9f2df48a2aae780cdaa6
wulawulu <wutuobangbang@gmail.com>
2024-12-25 10:34:49 +0800

    feature: macro_rules examples

---
+++ examples/my_try.rs
@@ -0,0 +1,29 @@
+use anyhow::{anyhow, Result};
+
+fn main() -> Result<()> {
+    let ret = my_try!(f3(my_try!(f2(my_try!(f1("hello"))))));
+    println!("Final result: {}", ret);
+    Ok(())
+}
+
+fn f1(s: impl AsRef<str>) -> Result<String> {
+    Ok(format!("f1: {}", s.as_ref()))
+}
+
+fn f2(s: impl AsRef<str>) -> Result<String> {
+    Ok(format!("f2: {}", s.as_ref()))
+}
+
+fn f3(s: impl AsRef<str>) -> Result<String> {
+    Err(anyhow!("f3: {}", s.as_ref()))
+}
+
+#[macro_export]
+macro_rules! my_try {
+    ($expr:expr) => {
+        match $expr {
+            Ok(val) => val,
+            Err(err) => return Err(err.into()),
+        }
+    };
+}
---
+++ examples/my_vec.rs
@@ -0,0 +1,29 @@
+use anyhow::Result;
+fn main() -> Result<()> {
+    // let v=vec![1,2,3,4,5];
+    let v: Vec<i32> = my_vec![
+        "1".parse()?,
+        "2".parse()?,
+        "3".parse()?,
+        "4".parse()?,
+        "5".parse()?,
+    ];
+    println!("{:?}", v);
+
+    Ok(())
+}
+
+#[macro_export]
+macro_rules! my_vec {
+    () =>{
+        Vec::new()
+    };
+    ($elem:expr; $n:expr) => {
+        std::vec::from_elem($elem, $n)
+    };
+    ($($x:expr),+ $(,)? ) => {{
+
+        <[_]>::into_vec(Box::new([$($x),+])
+        )
+        }};
+}
